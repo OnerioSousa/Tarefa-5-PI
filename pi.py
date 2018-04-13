05bb4b8ccd16a57db7b64cb46641b9256449dc2e
Onério Sousa <onerio@tribo.ppg.br>
2018-04-13 14:32:55 -0300

    correção de saida

--- pi.py
+++ pi.py
@@ -9,18 +9,20 @@
 
 def main():
 
-    numerador = 4; denominador = 0; fracao = 0; cont = 0;
+    numerador = 4; denominador = 1; fracao = 0; cont = 0;
 
-    for denominador in range(3,10000,2):
+    while abs(numerador/denominador) >= 0.0001:
        
         if cont%2==0:
             fracao = fracao + (numerador/denominador)
         else:
             fracao = fracao - (numerador/denominador)  
 
+        denominador = denominador+2;
+
         cont = cont +1;   
     
-    print('Valor PI %.4f '%(4-fracao));
+    print('Valor PI %.4f '%(fracao));
 
     return 0
 
